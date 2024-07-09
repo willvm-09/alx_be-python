d727ed4e1847e72160c2243a3e7a1b34ca36a140
willvm-09 <williammutugi7@gmail.com>
2024-07-09 16:43:02 +0300

    Rewrite code using vscode

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,23 +1,22 @@
 FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9
-CELSIUS_TO_FAHREINHEIT_FACTOR = 9/5
+CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
 
 def convert_to_celsius(fahrenheit):
-     result = fahreinheit - 32 * FAHRENHEIT_TO_CELSIUS_FACTOR
-     return result
+    celsius = (fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR
+    return celsius
 
-def convert_to_fahreinheit(celsius):
-     result = (celsius * CELSIUS_TO_FAHRENHEIT_FACTOR) + 32
-     return result
+def convert_to_fahrenheit(celsius):
+    fahreinheit = celsius * CELSIUS_TO_FAHRENHEIT_FACTOR + 32
+    return fahreinheit
 
 temperature = float(input("Enter the temperature to convert: "))
-symbol = input("Is this temperature in Celsius or Fahrenheit? (C/F): ").upper()
+unit = input("Is this temperature in Celsius or Fahreinheit? (C/F): ").strip().upper()
 
-if symbol == "F":
-    converted_temp = convert_to_celsius(temperature)
-    print(f"{temperature} is {converted_temp}C")
-elif symbol == "C":
-    converted_temp = convert_to_fahreinheit(temperature)
-    print(f"{temperature} is {converted_temp}F")
-else:
-     print("Invalid temperature. Please enter a numeric value.")
-
+if unit == "C":
+    converted_temperature = convert_to_fahrenheit(temperature)
+    print(f"{temperature}°C is {converted_temperature}°F")
+elif unit == "F":
+    converted_temperature = convert_to_celsius(temperature)
+    print(f"{temperature}°F is {converted_temperature}°C")
+else: 
+    print("Invalid temperature. Please enter a numeric value.")
