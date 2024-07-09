8a5036fe49dcc8bf265167871f87dbd0d2086d29
willvm-09 <williammutugi7@gmail.com>
2024-07-09 12:53:35 +0300

    Update Choice 1 Input message

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -14,7 +14,7 @@ def main():
         choice = input("Enter your choice: ")
 
         if choice == '1':
-            new_item = input("Enter item to add: ")
+            new_item = input("Enter the item to add: ")
             shopping_list.append(new_item)
             pass
         elif choice == '2':
