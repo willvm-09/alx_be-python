9c059ffc498834d6a28b39182b4ad95ed9aa2c41
willvm-09 <williammutugi7@gmail.com>
2024-07-14 17:21:24 +0300

    Add variables to code

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -1,15 +1,14 @@
 def safe_divide(numerator, denominator):
     try:
+        numerator = float(numerator)
+        denominator = float(denominator)
         result = numerator/denominator
     except ZeroDivisionError:
         print(f"Error: Cannot divide by zero.")
-
-    try: 
-        result2 = float(numerator)/float(denominator)
     except ValueError:
         print(f"Error: Please enter numeric values only.")
     else: 
-        print(f"The result of the division is {result2}")
+        print(f"The result of the division is {result}")
     finally:
         print(f"Division execution complete.")
     
