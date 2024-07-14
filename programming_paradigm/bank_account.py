9ec1b15ac4d6c54f287b983872af7551419f6a76
willvm-09 <williammutugi7@gmail.com>
2024-07-14 15:47:04 +0300

    Simple Bank Account Class

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,18 @@
+class BankAccount:
+    def __init__(self, account_balance = 0):
+        self.account_balance = account_balance
+
+    def deposit(self, amount):
+        deposit = amount + self.account_balance
+        return deposit
+    
+    def withdraw(self, amount):
+        if self.account_balance >= amount:
+            self.account_balance - amount
+            return True
+        else:
+            self.account_balance = self.account_balance
+            return False
+        
+    def display_balance(self):
+        print(f"Your Balance: {self.account_balance}")
