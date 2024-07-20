e92c23fe1331f60737c64e50acb433f39beff892
willvm-09 <williammutugi7@gmail.com>
2024-07-20 16:39:00 +0300

    Update Circle area method

--- oop/polymorphism_demo.py
+++ oop/polymorphism_demo.py
@@ -21,6 +21,6 @@ class Circle(Shape):
         self.radius = radius
 
     def area(self):
-        return math.pi * (self.radius * self.radius)
+        return math.pi * (self.radius ** 2)
     
         
